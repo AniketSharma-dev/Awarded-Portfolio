6c7f598a87d3893f3b1ccd83d5a3292084aaa852
AniketSharma-dev <aniketsharma.xo@gmail.com>
2024-10-30 23:38:26 +0530

    Loader Animation

--- app.js
+++ app.js
@@ -1,19 +1,49 @@
+function revealToSpan() {
+  const reveal = document.querySelectorAll(".reveal");
+
+  reveal.forEach((e) => {
+    let spanParent = document.createElement("span");
+    let spanChild = document.createElement("span");
+
+    spanParent.classList.add("parent");
+    spanChild.classList.add("child");
+
+    spanChild.innerHTML = e.innerHTML;
+
+    spanParent.appendChild(spanChild);
+    e.innerHTML = "";
+    e.appendChild(spanParent);
+  });
+}
+
+revealToSpan();
+
 let tl = gsap.timeline();
 
-tl.to(".fs", {
-  height: 0,
-  duration: 2,
-  ease: Expo.easeInOut,
+tl.from(".child span", {
+  left: "31%",
+  stagger: 0.2,
+  ease: Power3.easeInOut,
 })
-  .to(".elem", {
+  .to(".parent .child", {
+    y: "-100%",
+    duration: 0.4,
+    ease: Circ.easeInOut,
+  })
+  .to(".loader", {
+    height: 0,
+    duration: 1.1,
+    ease: Expo.easeInOut,
+  })
+  .to(".green", {
     height: "100%",
     delay: -2,
-    duration: 2,
-    ease: Expo.easeInOut,
+    duration: 1.1,
+    ease: Expo.easeIn,
   })
   .to(".white", {
     height: "100%",
-    delay: -1.65,
-    duration: 2,
+    delay: -1.4,
+    duration: 1.5,
     ease: Expo.easeInOut,
   });
