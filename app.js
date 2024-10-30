8adcfd56c68eff3c03b193fba9582d72a077f889
AniketSharma-dev <aniketsharma.xo@gmail.com>
2024-10-31 02:31:57 +0530

    Hero Section

--- app.js
+++ app.js
@@ -16,34 +16,46 @@ function revealToSpan() {
   });
 }
 
+function loaderAnimation() {
+  let tl = gsap.timeline();
+
+  tl.from(".child span", {
+    left: "41%",
+    stagger: 0.2,
+    ease: Power3.easeInOut,
+  })
+    .to(".parent .child", {
+      y: "-100%",
+      duration: 0.4,
+      ease: Circ.easeInOut,
+    })
+    .to(".loader", {
+      height: 0,
+      duration: 1.1,
+      ease: Expo.easeInOut,
+    })
+    .to(".green", {
+      height: "100%",
+      delay: -2,
+      duration: 1.1,
+      ease: Expo.easeIn,
+    })
+    .to(".white", {
+      height: "100%",
+      delay: -1.5,
+      duration: 1.5,
+      ease: Expo.easeInOut,
+    });
+}
+
+
+
 revealToSpan();
 
-let tl = gsap.timeline();
+loaderAnimation();
 
-tl.from(".child span", {
-  left: "31%",
-  stagger: 0.2,
-  ease: Power3.easeInOut,
-})
-  .to(".parent .child", {
-    y: "-100%",
-    duration: 0.4,
-    ease: Circ.easeInOut,
-  })
-  .to(".loader", {
-    height: 0,
-    duration: 1.1,
-    ease: Expo.easeInOut,
-  })
-  .to(".green", {
-    height: "100%",
-    delay: -2,
-    duration: 1.1,
-    ease: Expo.easeIn,
-  })
-  .to(".white", {
-    height: "100%",
-    delay: -1.4,
-    duration: 1.5,
-    ease: Expo.easeInOut,
-  });
+
+
+
+
+
